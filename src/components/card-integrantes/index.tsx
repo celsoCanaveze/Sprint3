f725edbdba1de20381e26c919dcba2968f19338c
Celso <32375695+celsoCanaveze@users.noreply.github.com>
2024-09-16 13:30:11 -0300

    Update index.tsx

--- src/components/card-integrantes/index.tsx
+++ src/components/card-integrantes/index.tsx
@@ -10,7 +10,7 @@ export function CardIntegrantes({name,github,imagem,rm}: CardIntegrantesProps){
         <div className="flex flex-row items-center gap-4 text-lg bg-zinc-800 rounded border border-zinc-200 p-5 w-full lg:w-[800px] justify-between">
             <img src= {imagem} alt="" className="size-28 lg:size-52 rounded-full"/>
             <aside className="flex flex-col gap-4">
-                <h1>{name}</h1>
+                <h1>{name} </h1>
                 <h1>RM: {rm}</h1>
                 <a target="_blank" href={github} className="px-4 py-2 bg-blue-500 rounded text-center hover:bg-blue-600 duration-200">Veja meu github</a>
             </aside>
